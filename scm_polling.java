c6b8aa3c050e9f623bdde59d4186e8294fcb6f7a
sebastinalex <sebastinalex@gmail.com>
2022-03-30 13:11:35 +0000

    scm commit

---
+++ scm_polling.java
@@ -0,0 +1,11 @@
+public class SebastinProject {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  //     System.out.println("Hello Pugazh");
+  //         System.out.println("Hello Magizh");
+  //             System.out.println("Hello Pugazh & Magizh");
+  //
+  //              }
+  //
+  //              }
